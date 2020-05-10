c4efa26b7b8655d020de96bcf8195ae89f68f054
Tanba28 <wa2.n893@gmail.com>
2020-05-11 00:08:18 +0900

    [add] add FreeRTOS LED Test

--- Test.cydsn/main.c
+++ Test.cydsn/main.c
@@ -10,17 +10,69 @@
  * ========================================
 */
 #include "project.h"
+#include "FreeRTOS.h"
+#include "task.h"
 
+void vTestTask1();
+void vTestTask2();
+void FreeRTOS_Setup();
 int main(void)
 {
     CyGlobalIntEnable; /* Enable global interrupts. */
 
     /* Place your initialization/startup code here (e.g. MyInst_Start()) */
 
+    FreeRTOS_Setup();
+    
+    xTaskCreate(vTestTask1,"test1",100,NULL,3,NULL);
+    xTaskCreate(vTestTask2,"test2",100,NULL,3,NULL);
+    
+    vTaskStartScheduler();
     for(;;)
     {
         /* Place your application code here. */
+
     }
+}
+
+void FreeRTOS_Setup(){
+    extern void xPortPendSVHandler( void );
+    extern void xPortSysTickHandler( void );
+    extern void vPortSVCHandler( void );
+    extern cyisraddress CyRamVectors[];
+
+    CyRamVectors[ 11 ] = ( cyisraddress ) vPortSVCHandler;
+    CyRamVectors[ 14 ] = ( cyisraddress ) xPortPendSVHandler;
+    CyRamVectors[ 15 ] = ( cyisraddress ) xPortSysTickHandler;
+}
+
+void vTestTask1(){
+    for(;;){
+        vTaskDelay(1000);
+        LED_1_Write(1);        
+    }    
+}
+//LEDを消灯させるタスク
+void vTestTask2(){
+    vTaskDelay(500);
+    for(;;){
+        vTaskDelay(1000);
+        LED_1_Write(0);        
+    }    
+}
+
+//オーバーフローが起こった際のハンドラ
+void vApplicationStackOverflowHook( TaskHandle_t pxTask, char *pcTaskName )
+{
+    /* The stack space has been execeeded for a task, considering allocating more. */
+    taskDISABLE_INTERRUPTS();
+}/*---------------------------------------------------------------------------*/
+
+//メモリの確保に失敗したときのハンドラ
+void vApplicationMallocFailedHook( void )
+{
+    /* The heap space has been execeeded. */
+    taskDISABLE_INTERRUPTS();
 }
 
 /* [] END OF FILE */
